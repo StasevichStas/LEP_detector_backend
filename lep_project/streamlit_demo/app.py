efd0f31849556489e30173c522e93a197a18989e
Stas Astapenko <z9538068100@gmail.com>
2026-01-29 13:51:02 +0700

    lep mvp v1

--- lep_project/streamlit_demo/app.py
+++ lep_project/streamlit_demo/app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
+import os
+from pathlib import Path
 
 st.title("Детектор изоляторов ЛЭП")
-
+base_dir = Path(__file__).resolve().parent.parent
 model_type = st.selectbox("Выберите модель", ["YOLOv8", "YOLOv11"])
 model_path = (
-    "best_weight_yolo8n.pt"
+    os.path.join(base_dir, "ml_models", "best_weight_yolo8n.pt")
     if model_type == "YOLOv8"
-    else "best_weight_yolo11n.pt"
+    else os.path.join(base_dir, "ml_models", "best_weight_yolo11n.pt")
 )
 
 
@@ -25,8 +27,6 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    results = model.predict(image)
+    results = model.predict(image, conf=0.25)
     res_plotted = results[0].plot()
-    st.image(
-        res_plotted, caption="Результат распознавания", width=700
-    )
+    st.image(res_plotted, caption="Результат распознавания", width=700)
